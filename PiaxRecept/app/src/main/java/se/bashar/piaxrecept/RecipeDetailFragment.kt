05905941e0722b44950bb17d5f1cfc523b14c102
Vice2 <90139249+Vice2@users.noreply.github.com>
2022-02-21 11:22:37 +0100

    CameraGallery

--- PiaxRecept/app/src/main/java/se/bashar/piaxrecept/RecipeDetailFragment.kt
+++ PiaxRecept/app/src/main/java/se/bashar/piaxrecept/RecipeDetailFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 
 class RecipeDetailFragment : Fragment() {
+
+    val model: RecipteVewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,5 +19,9 @@ class RecipeDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_recipe_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+
+    }
 }
